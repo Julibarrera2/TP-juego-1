6b2cef4b035bfd1302104598b8d8c5341aed6366
Miusmiau <164009952+Miusmiau@users.noreply.github.com>
2024-06-27 10:47:23 -0300

    funciones y variables publicas para paneles

--- Assets/Scripts/respuesta.cs
+++ Assets/Scripts/respuesta.cs
@@ -6,18 +6,23 @@ using UnityEngine.UI;
 public class respuesta : MonoBehaviour
 {
     public spawner spawner;
-    private int rta_valor;
     private int rta;
+    private int valor_correcto_rta;
     public InputField input_rta;
     public Button responder;
-    public Text txt_btn_rta;
+    public GameObject panel_rta;
+    public GameObject panel_corr_inc;
+    public Text txt_panel_corr_inc;
+    public Text txt_btn_reiniciar_juego;
+    public GameObject panel_no_rta;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rta_valor = int.Parse(input_rta.text);
-        rta = spawner.sumaPrecios;
+        rta = int.Parse(input_rta.text);
+        valor_correcto_rta = spawner.sumaPrecios;
     }
 
     // Update is called once per frame
@@ -26,11 +31,32 @@ public class respuesta : MonoBehaviour
         
     }
 
-    void chek_rta()
+    void click_chek_rta()
     {
-        if(rta_valor == rta)
+        if (input_rta.text == "")
         {
-
+            panel_no_rta.SetActive(true);
         }
+        else
+        {
+            panel_corr_inc.SetActive(true);
+            if (rta == valor_correcto_rta)
+            {
+                txt_panel_corr_inc.text = "correcto";
+                txt_btn_reiniciar_juego.text = "Reiniciar el desafio";
+
+            }
+            else
+            {
+                txt_panel_corr_inc.text = "incorrecto";
+                txt_btn_reiniciar_juego.text = "Volver a intentarlo";
+            }
+        }
+    }
+
+    void click_cerrar_panel_no_rta()
+    {
+        input_rta.text = "";
+        panel_no_rta.SetActive(false);
     }
 }
