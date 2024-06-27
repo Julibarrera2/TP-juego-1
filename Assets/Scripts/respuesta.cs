1647dec858f4af55b978638ff7793fa0ad7f612b
Miusmiau <164009952+Miusmiau@users.noreply.github.com>
2024-06-27 08:34:35 -0300

    srgrey

---
+++ Assets/Scripts/respuesta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class respuesta : MonoBehaviour
+{
+    public Button responder;
+    public InputField input_rta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rta_valor = int.Parse(input_rta.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void chek_rta()
+    {
+        if()
+    }
+}
